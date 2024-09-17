c365b1878730371a1c93884a60dad2c53f3f44f2
ekolesnyk <eugene.kolesnyk@customertimes.com>
2024-09-17 17:43:15 +0200

    feat: added tailwind and shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
+import path from 'node:path';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	vite: {
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
